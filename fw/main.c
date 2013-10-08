ddce7fe0257dc254b035f7964016a6a5555da65e
Tomas Hluchan <tomas.hluchan@gmail.com>
2013-10-08 13:23:46 +0200

    melody test

--- fw/main.c
+++ fw/main.c
@@ -1,6 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
+#include "pitches.h"
 
 #define BUZ 0
 #define LED 1
@@ -34,12 +34,60 @@ void beep(int f, int d) {
 	}
 }
 
+//#include "melody_mario.h"
+#include "melody_bond.h"
 
-int main2(void)
-{
-    DDRB |= _BV(LED);
-    LED_ON;
+void delay_us(long d) {
+	while(d>100) {
+		_delay_us(100);
+		d-=100;	
+	}
+	while(d>10) {
+		_delay_us(10);
+		d-=10;	
+	}
+	while(d>0) {
+		_delay_us(1);
+		d-=1;
+	}
 }
+void buzz(long frequency, long length) {
+
+
+  if (frequency) {
+	  long delayValue = 1000000/frequency/2; 
+	  long numCycles = frequency * length/ 1000; 
+ 	 for (long i=0; i < numCycles; i++){ 
+  	  SPK_ON;
+   	 delay_us(delayValue); 
+   	 SPK_OFF;
+   	 delay_us(delayValue); 
+ 	 }
+ } else {
+	  long numCycles = length; 
+ 	  for (long i=0; i < numCycles; i++){ 
+		_delay_ms(1);
+	  }
+ }
+}
+
+
+
+
+void play() {
+	for (int thisNote = 0; thisNote < size; thisNote++) {
+
+       		int noteDuration = 1000/tempo[thisNote];
+
+	      	buzz(melody[thisNote],noteDuration);
+
+       	//	int pauseBetweenNotes = noteDuration * 1.30;
+       	//	delay(pauseBetweenNotes);
+
+	}
+}
+
+
 int main(void)
 {
     DDRB |= _BV(LED);
@@ -63,16 +111,17 @@ int main(void)
 	//	LED_ON;
         } else {
     		flash_led(2);
-		beep(1,5);
+		play();
     		flash_led(2);
         }	
         if (PINB & _BV(BUZ)) {
 	//	LED_ON;
         } else {
 		LED_ON;
-		beep(1,5);
+//		beep(1,5);
+		play();
 		LED_OFF;
-		beep(1,5);
+//		beep(1,5);
         }	
  
     }
